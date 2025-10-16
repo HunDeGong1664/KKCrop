5435777ca5ef8fff2a5f384b93a72c19dec5c834
HunDegong <691325380@qq.com>
2025-10-16 18:09:24 +0800

    [Update]: 修复打包失败、第三方库加载失败等问题；

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      // 配置rollup处理外部依赖
+      external: ['sharp', 'pdf-lib'], // 将sharp和pdf-lib都标记为外部依赖，避免打包进bundle
+      plugins: []
+    }
+  }
+});
